da21d939def991468b315ddf689d9d07b079e4b0
bagasajaa19 <bagas.prasetyo_ti24@nusaputra.ac.id>
2025-10-22 20:30:22 +0700

    Node.js

---
+++ modules/convert.js
@@ -0,0 +1,7 @@
+function celciusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+function kilometersToMeters(kilometers) {
+    return kilometers * 1000;
+}
+module.exports = { celciusToFahrenheit, kilometersToMeters };
