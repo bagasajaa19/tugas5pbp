1cfe4f61127a35fd746e49516eda8cc3ec6506ba
bagasajaa19 <bagas.prasetyo_ti24@nusaputra.ac.id>
2025-10-22 20:31:19 +0700

    Delete modules/convert.js

--- modules/convert.js
+++
@@ -1,7 +0,0 @@
-function celciusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-function kilometersToMeters(kilometers) {
-    return kilometers * 1000;
-}
-module.exports = { celciusToFahrenheit, kilometersToMeters };
